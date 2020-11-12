41792e4b655079bec2de56150fea8f5b5bf324b0
unknown <you@example.com>
2020-11-12 18:12:53 +0700

    fix db seed

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Models\Category;
 use App\Models\Setting;
+use App\Models\Slider;
 use Illuminate\Database\Seeder;
 use Spatie\Permission\Models\Permission;
 use Spatie\Permission\Models\Role;
@@ -16,7 +18,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $settings =[
+        // SETTING TABLES
+        $settings = [
             ['config_key' => 'facebook',     'config_value' => 'https://www.facebook.com/ducnam0109/'],
             ['config_key' => 'twitter',     'config_value' => 'https://www.twitter.com/ducnam0109/'],
             ['config_key' => 'linkedin',     'config_value' => 'https://www.linkedin.com/ducnam0109/'],
@@ -29,8 +32,7 @@ class DatabaseSeeder extends Seeder
             Setting::create($setting);
         };
 
-
-
+        // SETTING STAFF
         $admin = Staff::create(
             [
                 'name' => 'admin',
@@ -39,37 +41,37 @@ class DatabaseSeeder extends Seeder
             ]
         );
 
-        // // create permissions
-        // Permission::create(['name' => 'edit category']);
-        // Permission::create(['name' => 'delete category']);
-        // Permission::create(['name' => 'add category']);
+        // SETTING CATEGORY
+        $categories = [
+            ['name' => 'SPORTSWEAR', 'slug' => 'men',     'parent_id' => 0],
+            ['name' => 'MENS', 'slug' => 'women',     'parent_id' => 0],
+            ['name' => 'WOMENS', 'slug' => 'kid',     'parent_id' => 0],
+            ['name' => 'KIDS', 'slug' => 'winter',     'parent_id' => 0],
+            ['name' => 'FASHION', 'slug' => 'autums',     'parent_id' => 0],
+            ['name' => 'HOUSEHOLDS', 'slug' => 'autums',     'parent_id' => 0],
+            ['name' => 'INTERIORS', 'slug' => 'autums',     'parent_id' => 0],
+            ['name' => 'NIKE', 'slug' => 'autums',     'parent_id' => 1],
+            ['name' => 'UNDER ARMOUR', 'slug' => 'autums',     'parent_id' => 1],
+            ['name' => 'ADIDAS', 'slug' => 'autums',     'parent_id' => 1],
+            ['name' => 'PUMA', 'slug' => 'autums',     'parent_id' => 1],
+            ['name' => 'FENDI', 'slug' => 'autums',     'parent_id' => 1],
+            ['name' => 'VALENTINO', 'slug' => 'autums',     'parent_id' => 1],
+            ['name' => 'DIOR', 'slug' => 'autums',     'parent_id' => 2],
+            ['name' => 'VERSACE', 'slug' => 'autums',     'parent_id' => 2],
+            ['name' => 'GUESS', 'slug' => 'autums',     'parent_id' => 3],
 
-        // // create permissions
-        // Permission::create(['name' => 'check order']);
-        // Permission::create(['name' => 'comfirm order']);
-        // Permission::create(['name' => 'cancel order']);
+        ];
+        foreach ($categories as  $categorie) {
+            Category::create($categorie);
+        };
 
-        // // create permissions
-        // Permission::create(['name' => 'edit product']);
-        // Permission::create(['name' => 'delete product']);
-        // Permission::create(['name' => 'add product']);
-
-        // // create permissions
-        // Permission::create(['name' => 'edit role']);
-        // Permission::create(['name' => 'delete role']);
-        // Permission::create(['name' => 'add role']);
-
-        // // // or may be done by chaining
-        // $role = Role::create(['name' => 'staff'])
-        //     ->givePermissionTo(['check order', 'check order']);
-
-        // // // or may be done by chaining
-        // // $roleadmin = Role::create(['name' => 'admin'])
-        // //     ->givePermissionTo(['edit category', 'add category', 'edit product', 'add product', 'delete product']);
-
-
-        // // $rolesuper = Role::create(['name' => 'super-admin']);
-        // // $rolesuper->givePermissionTo(Permission::all());
-        // // $admin->syncRoles($rolesuper);
+        // SETTING TABLES
+        $sliders = [
+            ['name' => 'ƯU ĐÃI KHỦNG NGÀY 12 tháng 5!!', 'description' => ' Từ ngày 20/3 giảm giá tất cả các mạt ang túi xách', 'image_path' => '/storage/slider/1/default.jpg',     'image_name' => 'default.jpg'],
+            ['name' => 'Đặt hàng vinsmart lãi suất 0%', 'description' => ' Từ ngày 20/3 giảm giá tất cả các mạt ang túi xách', 'image_path' => '/storage/slider/1/default1.jpg',     'image_name' => 'default1.jpg'],
+        ];
+        foreach ($sliders as  $slider) {
+            Slider::create($slider);
+        };
     }
 }
