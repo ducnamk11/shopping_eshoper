fc5c8c0f4925218a5ecd61def081cebc3e4ded01
unknown <you@example.com>
2020-11-12 09:29:47 +0700

    add review column

---
+++ app/Models/Review.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use App\User;
+use Illuminate\Database\Eloquent\Model;
+
+class Review extends Model
+{
+    protected $guarded = [];
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
